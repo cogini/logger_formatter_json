835067e3937ee3a744f78273fe9f3f518d00acc6
Jake Morrison <jake@cogini.com>
2022-09-04 15:07:47 -0500

    Convert iolist to binary

--- test/logger_formatter_json_SUITE.erl
+++ test/logger_formatter_json_SUITE.erl
@@ -11,26 +11,34 @@ unstructured() ->
     [{docs, "logs that aren't structured get passed through with a re-frame"}].
 unstructured(_) ->
     ?assertEqual(
-       <<"{\"syslog.severity\":\"info\",\"message\":\"abc\"}">>,
-       logger_formatter_json:format(#{level => info, msg => {string, "abc"},
-                                      meta => #{}}, #{})),
+       <<"{\"syslog.severity\":\"info\",\"message\":\"abc\"}\n">>,
+       iolist_to_binary(
+         logger_formatter_json:format(#{level => info, msg => {string, "abc"},
+                                        meta => #{}}, #{}))
+      ),
     ?assertEqual(
-       <<"{\"syslog.severity\":\"info\",\"message\":\"abc\"}">>,
-       logger_formatter_json:format(#{level => info,
-                                      msg => {string, [<<"abc">>]},
-                                      meta => #{}}, #{})
-    ),
+       <<"{\"syslog.severity\":\"info\",\"message\":\"abc\"}\n">>,
+       iolist_to_binary(
+         logger_formatter_json:format(#{level => info,
+                                        msg => {string, [<<"abc">>]},
+                                        meta => #{}}, #{})
+        )
+      ),
     ?assertEqual(
-       <<"{\"syslog.severity\":\"info\",\"message\":\"hello world\"}">>,
-       logger_formatter_json:format(#{level => info,
-                                      msg => {"hello ~s", ["world"]},
-                                      meta => #{}}, #{})
-    ),
+       <<"{\"syslog.severity\":\"info\",\"message\":\"hello world\"}\n">>,
+       iolist_to_binary(
+         logger_formatter_json:format(#{level => info,
+                                        msg => {"hello ~s", ["world"]},
+                                        meta => #{}}, #{})
+        )
+      ),
     ok.
 
 basic(_) ->
     ?assertEqual(
-       <<"{\"syslog.severity\":\"info\",\"hi\":\"there\"}">>,
-       logger_formatter_json:format(#{level => info, msg => {report, #{hi => there}}, meta => #{}}, #{})
-    ),
+       <<"{\"syslog.severity\":\"info\",\"hi\":\"there\"}\n">>,
+       iolist_to_binary(
+         logger_formatter_json:format(#{level => info, msg => {report, #{hi => there}}, meta => #{}}, #{})
+        )
+      ),
     ok.
