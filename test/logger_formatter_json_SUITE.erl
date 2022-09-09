29d4b779f3b26ec6529c945f66a8513bf3f0c5e0
Jake Morrison <jake@cogini.com>
2022-09-09 08:48:48 -0500

    rebar3 format

--- test/logger_formatter_json_SUITE.erl
+++ test/logger_formatter_json_SUITE.erl
@@ -1,6 +1,7 @@
 -module(logger_formatter_json_SUITE).
 
 -compile(export_all).
+
 -include_lib("common_test/include/ct.hrl").
 -include_lib("stdlib/include/assert.hrl").
 
@@ -9,48 +10,38 @@ all() ->
 
 unstructured() ->
     [{docs, "logs that aren't structured get passed through with a re-frame"}].
+
 unstructured(_) ->
-    ?assertEqual(
-       <<"{\"msg\":\"abc\",\"level\":\"info\"}\n">>,
-       iolist_to_binary(
-         logger_formatter_json:format(#{level => info, msg => {string, "abc"},
-                                        meta => #{}}, #{}))
-      ),
-    ?assertEqual(
-       <<"{\"msg\":\"abc\",\"level\":\"info\"}\n">>,
-       iolist_to_binary(
-         logger_formatter_json:format(#{level => info,
-                                        msg => {string, [<<"abc">>]},
-                                        meta => #{}}, #{})
-        )
-      ),
-    ?assertEqual(
-       <<"{\"msg\":\"hello world\",\"level\":\"info\"}\n">>,
-       iolist_to_binary(
-         logger_formatter_json:format(#{level => info,
-                                        msg => {"hello ~s", ["world"]},
-                                        meta => #{}}, #{})
-        )
-      ),
+    ?assertEqual(<<"{\"msg\":\"abc\",\"level\":\"info\"}\n">>,
+                 iolist_to_binary(logger_formatter_json:format(#{level => info,
+                                                                 msg => {string, "abc"},
+                                                                 meta => #{}},
+                                                               #{}))),
+    ?assertEqual(<<"{\"msg\":\"abc\",\"level\":\"info\"}\n">>,
+                 iolist_to_binary(logger_formatter_json:format(#{level => info,
+                                                                 msg => {string, [<<"abc">>]},
+                                                                 meta => #{}},
+                                                               #{}))),
+    ?assertEqual(<<"{\"msg\":\"hello world\",\"level\":\"info\"}\n">>,
+                 iolist_to_binary(logger_formatter_json:format(#{level => info,
+                                                                 msg => {"hello ~s", ["world"]},
+                                                                 meta => #{}},
+                                                               #{}))),
     ok.
 
 structured(_) ->
-    ?assertEqual(
-       <<"{\"hi\":\"there\",\"level\":\"info\"}\n">>,
-       iolist_to_binary(
-         logger_formatter_json:format(#{level => info, msg => {report, #{hi => there}}, meta => #{}}, #{})
-        )
-      ),
+    ?assertEqual(<<"{\"hi\":\"there\",\"level\":\"info\"}\n">>,
+                 iolist_to_binary(logger_formatter_json:format(#{level => info,
+                                                                 msg => {report, #{hi => there}},
+                                                                 meta => #{}},
+                                                               #{}))),
     ok.
 
 metadata(_) ->
-    Config = #{
-               names => datadog
-              },
-    ?assertEqual(
-       <<"{\"message\":\"abc\",\"status\":\"info\"}\n">>,
-       iolist_to_binary(
-         logger_formatter_json:format(#{level => info, msg => {string, "abc"},
-                                        meta => #{}}, Config))
-      ),
+    Config = #{names => datadog},
+    ?assertEqual(<<"{\"message\":\"abc\",\"status\":\"info\"}\n">>,
+                 iolist_to_binary(logger_formatter_json:format(#{level => info,
+                                                                 msg => {string, "abc"},
+                                                                 meta => #{}},
+                                                               Config))),
     ok.
